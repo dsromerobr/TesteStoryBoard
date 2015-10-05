7eda7033bcc848f39fc73f643a0b6babe153af3e
Douglas Romero <douglasromero@macminiromero.local>
2015-10-05 17:25:22 -0300

    Initial Commit

---
+++ TesteStoryBoard/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  TesteStoryBoard
+//
+//  Created by Douglas Romero on 05/10/15.
+//  Copyright © 2015 Douglas Romero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ TesteStoryBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TesteStoryBoard
+//
+//  Created by Douglas Romero on 05/10/15.
+//  Copyright © 2015 Douglas Romero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
